520ee984276cb83b58c5933785f943915b5a83d4
Marjorie-J <janda.marjorie@gmail.com>
2025-05-22 16:52:44 +0200

    first commit

---
+++ Bloc_1_Kayak/variables.py
@@ -0,0 +1,18 @@
+best_cities = ["Mont Saint Michel", "Saint Malo", "Bayeux", "Le Havre", "Rouen", "Paris",
+"Amiens", "Lille", "Strasbourg", "Chateau du Haut Koenigsbourg", "Colmar", "Eguisheim",
+"Besancon", "Dijon", "Annecy", "Grenoble", "Lyon", "Gorges du Verdon", "Bormes les Mimosas",
+"Cassis", "Marseille", "Aix en Provence", "Avignon", "Uzes", "Nimes", "Aigues Mortes",
+"Saintes Maries de la mer", "Collioure", "Carcassonne", "Ariege", "Toulouse", "Montauban",
+"Biarritz", "Bayonne", "La Rochelle"]
+
+datas_dir = "datas/"
+map_dir = "maps/"
+
+cities_csv_name = datas_dir + "cities.csv"
+hotels_jl_name = datas_dir + "hotels.jl"
+weather_csv_name = datas_dir + "weather.csv"
+bucket_csv_name = "final_cities_hotels_weather.csv"
+final_csv_name = datas_dir + bucket_csv_name
+
+weather_html_map = map_dir + "weather_map.html"
+hotel_html_map = map_dir + "hotel_map.html"
